3f5b751d5321729d85c5b5c5c1d9bf6e412352f9
Joao Pedro <jp.bruno.machado@gmail.com>
2025-03-01 13:54:48 -0300

    diff prod to dev db env

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,12 +1,21 @@
 import mysql from "mysql2";
 
 const pool = mysql
-    .createPool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PW,
-        database: process.env.DB_NAME,
-    })
+    .createPool(
+        process.env.NODE_ENV === "production"
+            ? {
+                  host: process.env.DB_HOST,
+                  user: process.env.DB_USER,
+                  password: process.env.DB_PW,
+                  database: process.env.DB_NAME,
+              }
+            : {
+                  host: process.env.DB_HOST_DEV,
+                  user: process.env.DB_USER_DEV,
+                  password: process.env.DB_PW_DEV,
+                  database: process.env.DB_NAME_DEV,
+              }
+    )
     .promise();
 
 export default pool;
