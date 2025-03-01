37057231cf690a830369cb48f80ed4bd5d190c4c
Joao Pedro <jp.bruno.machado@gmail.com>
2025-03-01 15:24:05 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,6 @@ const cookieSettings: CookieOptions =
     process.env.NODE_ENV === "production"
         ? {
               sameSite: "strict", //as the server doesn't have the same domain as the client
-              secure: true,
               maxAge: 3600000 * 24, //one day
               httpOnly: true,
           }
