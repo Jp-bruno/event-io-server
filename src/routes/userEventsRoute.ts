2a6fcdb6cd41fb5bdfe440c958a1fe99f11eb6f4
Joao Pedro <jp.bruno.machado@gmail.com>
2025-03-05 13:15:10 -0300

    unenroll user func name refac

--- src/routes/userEventsRoute.ts
+++ src/routes/userEventsRoute.ts
@@ -4,6 +4,7 @@ import userEventsContoller from "../controllers/userEventsContoller.js";
 const userEventsRoute = Router()
 
 userEventsRoute.post("/", (req, res) => userEventsContoller.enrollUser(req, res))
+userEventsRoute.delete("/:userId/:eventId", (req, res) => userEventsContoller.unenrollUser(req, res))
 userEventsRoute.get("/", (req, res) => userEventsContoller.getUserEvents(req, res))
 
 export default userEventsRoute;
